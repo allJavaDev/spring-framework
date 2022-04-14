57aba7c89a56023db6fd0c369235f3248c70a4e8
zk <zuhalkaled@gmail.com>
2022-04-14 15:42:26 -0400

    ORM create tables

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/alljavadev/entity/Employee.java
@@ -0,0 +1,12 @@
+package com.alljavadev.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    private int id;
+    private String name;
+
+}
